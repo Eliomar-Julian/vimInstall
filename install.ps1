bb3272d5aa45868abcceaaa70d3c3917e60e9721
Eliomar-Julian <eliomarjulian001@gmail.com>
2020-12-04 14:13:20 -0300

    documentação

--- install.ps1
+++ install.ps1
@@ -9,6 +9,7 @@
         New-Item -Path "C:\$user\vimfiles\" -Name "autoload" -ItemType "directory"
         Move-Item -Path ".\vimrc" -Destination "C:\$user\vimfiles"
         Move-Item -Path ".\plugvim" -Destination "C:\$user\vimfiles\autoload"
+        Move-Item -Path ".\monokai.vim" -Destination "C:\tools\vim\vim82\colors"
     }
 }
 
@@ -22,7 +23,6 @@ catch
     Set-ExecutionPolicy Bypass -Scope Process -Force
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    cls
+    Clear-Host
     Install
-
 }
